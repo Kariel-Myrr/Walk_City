125aa8b4a58d4122847f432e4d1ec57258f25256
Mr_Magik <kirilant2013@gmail.com>
2018-11-30 16:47:45 +0300

    Это стартовая версия с макетом

---
+++ app/src/main/java/com/example/lesson_1/walkcity/Game_Map.kt
@@ -0,0 +1,12 @@
+package com.example.lesson_1.walkcity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Game_Map : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game__map)
+    }
+}
---
+++ app/src/main/java/com/example/lesson_1/walkcity/Menu.kt
@@ -0,0 +1,26 @@
+package com.example.lesson_1.walkcity
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_menu.*
+
+class Menu : AppCompatActivity() {
+    fun changeActtoSettings(demo : View){
+        val intent = Intent(this,Settings::class.java)
+        startActivity(intent)
+
+    }
+    fun changeActtoGame_Map(demo : View){
+        val intent = Intent(this,Game_Map::class.java)
+        startActivity(intent)
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+        play.setOnClickListener(::changeActtoGame_Map)
+        settings.setOnClickListener(::changeActtoSettings)
+    }
+}
