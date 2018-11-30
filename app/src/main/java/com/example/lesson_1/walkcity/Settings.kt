8ebd86d42e3af833090696e2d3bff4a300bd8d88
Mr_Magik <kirilant2013@gmail.com>
2018-11-30 16:51:48 +0300

    Это стартовая версия с макетом

--- app/src/main/java/com/example/lesson_1/walkcity/Settings.kt
+++ app/src/main/java/com/example/lesson_1/walkcity/Settings.kt
@@ -8,5 +8,6 @@ class Settings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        
     }
 }
