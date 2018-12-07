73bf5fb7e4ab8489e4f487530168436016d8b334
Mr_Magik <kirilant2013@gmail.com>
2018-12-07 15:02:19 +0300

    Дебаг стартовой

--- app/src/main/java/com/example/lesson_1/walkcity/MainActivity.kt
+++ app/src/main/java/com/example/lesson_1/walkcity/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
 
     fun cliuck(demo : View){
